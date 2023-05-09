4171d0d5c4e36da5db0060cf570b9acf601a5d42
user <jeffdplante@aol.com>
2023-05-09 16:08:45 -0700

    Add selectedDomain state to control the display

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -2,6 +2,7 @@ import data from '../data/data.json';
 import DomainsCarousel from './DomainsCarousel';
 
 function Body() {
+
   return (
     <div  className="h-full ml-16 w-full inline-block bg-gray-700 flex flex-col">
       <DomainsCarousel data={data}/>
--- src/components/DomainsCarousel.tsx
+++ src/components/DomainsCarousel.tsx
@@ -1,19 +1,28 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 function DomainsCarousel ({ data }) {
+  const [selectedDomain, setSelectedDomain] = useState(null);
+
+  useEffect(() => {console.log(selectedDomain)},[selectedDomain])
   return (
     <div>
       <ul className="flex flex-row gap-2 justify-center mt-16">
         {data.map((domain, idx)=> (
-          <DomainButton key={idx} name={domain.domain} />
+          <DomainButton setSelectedDomain={setSelectedDomain} key={idx} idx={idx} name={domain.domain} />
         ))}
       </ul>
     </div>
   );
 }
 
-const DomainButton = ({ name }) => {
+const DomainButton = ({ setSelectedDomain, idx, name }) => {
+  function handleClick() {
+    setSelectedDomain(idx);
+  }
+
   return (
-    <button className="shadow-lg text-gray-200 focus:text-red-500 focus:text-2xl bg-gray-500 hover:bg-gray-600 px-6 py-4 rounded-lg text-xl">{name}</button>
+    <button onClick={handleClick} className="shadow-lg text-gray-200 focus:text-red-500 focus:text-2xl bg-gray-500 hover:bg-gray-600 px-6 py-4 rounded-lg text-xl">{name}</button>
   )
 }
 
