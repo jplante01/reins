1545511f01b2b6b5c1afb6e8b33afcd4910cea75
user <jeffdplante@aol.com>
2023-05-30 15:43:06 -0700

    Render list of Domains

--- src/routes/TasksView.jsx
+++ src/routes/TasksView.jsx
@@ -10,8 +10,34 @@ function TasksView() {
 }
 
 function Domains() {
-  const dataObj = useContext(DataContext);
-  console.log(dataObj);
+  const { dataObj, setDataObj } = useContext(DataContext);
+  console.log([dataObj, setDataObj]);
+  return (
+    <div>
+      <ul className="flex flex-row gap-2 justify-center mt-16">
+        {dataObj.map((domain, idx) => (
+          <DomainButton
+            key={domain.domainName}
+            idx={idx}
+            name={domain.domainName}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function DomainButton({ idx, name }) {
+  const selectedDomain = 'test';
+  return (
+    <button
+      className={`shadow-lg text-gray-200 ${
+        selectedDomain === idx ? 'text-red-500' : ''
+      } bg-gray-500 hover:bg-gray-600 px-6 py-4 rounded-lg text-xl`}
+    >
+      {name}
+    </button>
+  );
 }
 
 export default TasksView;
